f5bf4ef74179afab09c7dd8876676e1342638ddd
Siddaji <siddaji874@gmail.com>
2025-09-03 21:30:15 +0530

    username and email validation in loginUser

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -109,13 +109,15 @@ const loginUser=asyncHandler(async(req,res)=>{
 
   const {email,username,password}=req.body
 
-  if (!username || !email) {
+  if (!(username || email)) {
     throw new ApiError(400,"username or password is required");
   }
 
   const user=await User.findOne({
-    $or:[{username},{email}]
-  })
+      $or:[{username},{email}]
+    })
+      console.log("Found user:",user);
+                                                       
 
   if (!user) {
     throw new ApiError(404,"User does not exists")
@@ -155,12 +157,34 @@ const loginUser=asyncHandler(async(req,res)=>{
 
 
 const logOutUser=asyncHandler(async(req,res)=>{
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set:{
+        refreshToken:undefined
+      }
+    },
+    {
+      new:true
+    }
+    
+  )
+  const options={
+    httpOnly:true,
+    secure:true
+
+  }
+  return res
+  .status(200)
+  .clearCookkie("accessToken",options)
+  .clearCookkie("refreshToken",options)
+  .json(new ApiResponse(200,{},"User logout Successfully"))
   
 
 })
 
 export {registerUser,
-  loginUser
+  loginUser,logOutUser
 }
 
 
